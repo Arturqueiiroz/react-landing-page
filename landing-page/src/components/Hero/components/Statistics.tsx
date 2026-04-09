e5d3051245069ab990564b9a1cd7a6622ab231a3
ARTUR COSMO DE QUEIROZ <artur.cqueiroz@senacsp.edu.br>
2026-04-09 17:20:13 -0300

    first commit

---
+++ landing-page/src/components/Hero/components/Statistics.tsx
@@ -0,0 +1,17 @@
+import './Statistics.css'
+
+interface StatisticProps {
+    data: String;
+    description: string;
+}
+
+function Statistic({data, description}: StatisticProps) {
+    return (
+        <div className="stats">
+            <h2>{data}</h2>
+            <p>{description}</p>
+        </div>
+    );
+}
+
+export default Statistic;
---
+++ landing-page/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+//Barra de navagação fixa no topo da página
+import Button from '../Button/Button';
+import './style.css';
+
+function showMessage(){
+    alert('Realizar login')
+}
+
+function Navbar(){
+    return(
+        <nav className="navbar">
+
+            <div className="container">
+                <a href="#" className="logo">EduTrack</a>
+
+                <ul className="list-menu">
+                    <li><a className="link" href="#sobre">Sobre</a></li>
+                    <li><a className="link" href="#funcionalidades">Funcionalidades</a></li>
+                    <li><a className="link" href="#equipe">Equipe</a></li>
+                    <li><a className="link" href="#contato">Contato</a></li>
+                </ul>
+
+                <div>
+                    <Button border={false} filled={false} label='Login' onClick={showMessage}/>
+                    <Button border={true} filled={true} label='Começar agora' onClick={() => {}}/>
+                </div>
+
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
